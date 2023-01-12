39c1e3c8be8ad9c4db6e67aa4142d6163f29b583
Atley St. Vrain <atleystvrain@gmail.com>
2023-01-12 12:16:45 -0600

    RollLoadedDie Complete

--- RollLoadedDie.java
+++ RollLoadedDie.java
@@ -1,5 +1,20 @@
 public class RollLoadedDie {
     public static void main(String[] args) {
+        double rand = Math.random();
+
+        if (rand <= .125) {
+            System.out.println("1");
+        } else if (rand<=.25 && rand>.125) {
+            System.out.println("2");
+        } else if (rand<=.375 && rand>.25) {
+            System.out.println("3");
+        } else if (rand<=.5 && rand>.375){
+            System.out.println("4");
+        } else if (rand<=.625 && rand>.5){
+            System.out.println("5");
+        } else if (rand>.625){
+            System.out.println("6");
+        }
         
     }
 }
