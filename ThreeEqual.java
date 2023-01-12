534b35e7d0e9f58c576abe9429ae257c257cba96
Atley St. Vrain <atleystvrain@gmail.com>
2023-01-12 11:38:34 -0600

    ThreeEqual complete

--- ThreeEqual.java
+++ ThreeEqual.java
@@ -1,5 +1,23 @@
 public class ThreeEqual {
     public static void main(String[] args) {
         
+
+        if (args.length < 3) {
+            System.out.println("Please provide three numbers");
+        }else {
+            int a = Integer.parseInt(args[0]);
+            int b = Integer.parseInt(args[1]);
+            int c = Integer.parseInt(args[2]);
+            if (a == b){
+                if (b == c){
+                    System.out.println("equal");
+                } else{
+                    System.out.println("not equal");
+                }
+            } else{
+                System.out.println("not equal");
+            }
+        }
+        
     }
 }
